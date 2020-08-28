859998f01b0ec4745efedb32c09480d738f06fa4
Romanov5000 <romanovvladislav97@gmail.com>
2020-08-28 16:44:18 +0300

    add heroku

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App';
 
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 80
+app.listen(PORT)
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
