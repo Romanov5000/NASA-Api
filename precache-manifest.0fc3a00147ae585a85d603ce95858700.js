f53e79f4978a98a8249aeb9d82bce0f011e21481
Romanov5000 <romanovvladislav97@gmail.com>
2020-11-07 23:13:32 +0300

    Updates

---
+++ precache-manifest.0fc3a00147ae585a85d603ce95858700.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71020cb51ad785b25ebafd8ddc47a643",
+    "url": "/NASA-Api/index.html"
+  },
+  {
+    "revision": "e41cb325d79ac4230ef5",
+    "url": "/NASA-Api/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "a9f5fe2eba64c31d067f",
+    "url": "/NASA-Api/static/css/main.fccb934e.chunk.css"
+  },
+  {
+    "revision": "e41cb325d79ac4230ef5",
+    "url": "/NASA-Api/static/js/2.27a53fd6.chunk.js"
+  },
+  {
+    "revision": "aeacff1fc9d7c4606fe6c6435329b18f",
+    "url": "/NASA-Api/static/js/2.27a53fd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9f5fe2eba64c31d067f",
+    "url": "/NASA-Api/static/js/main.d060ac6b.chunk.js"
+  },
+  {
+    "revision": "fc439b3bd1ccbaa564d6",
+    "url": "/NASA-Api/static/js/runtime-main.192ed46b.js"
+  }
+]);
